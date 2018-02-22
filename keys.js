11b056b9a3ad58f76bbd603560e3fd6707b50347
rupali.meshram <meshram.rupali86@gmail.com>
2018-02-22 11:54:47 -0500

    liri-bot

--- keys.js
+++ keys.js
@@ -1,6 +1,6 @@
 
 
-exports.twitter = {
+/*exports.twitter = {
   consumer_key: 'eg5PKKTKngcPOQ9MXQC7tlqMf',
   consumer_secret: '5BHKA8rxlyQJDVMBzgRI7sK0kAid6VfyjbOHnSERe1J7nfu5ss',
   access_token_key: '965967599887626240-PXpIj7uaBN9ZhGKo9aXEOyIepJF1cAx',
@@ -11,4 +11,18 @@ exports.twitter = {
 exports.spotify = {
   id: '6641e75b9e9941659c928899dd22d109',
   secret: '6691acf379f245a9a26093eab2e3d4a7'
+};*/
+
+console.log('this is loaded');
+
+exports.twitter = {
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+};
+
+exports.spotify = {
+  id: process.env.SPOTIFY_ID,
+  secret: process.env.SPOTIFY_SECRET
 };
